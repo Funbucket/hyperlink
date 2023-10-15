37b86182632c5c7e7e83b372d630271839066862
조해창 <outofchng@gmail.com>
2023-10-16 01:49:55 +0900

    GTM 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
+import GTM from '~/components/GTM'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GTM />
+      <Component {...pageProps} />
+    </>
+  )
 }
